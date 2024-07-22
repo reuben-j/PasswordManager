d8fa1155d352bc3b371720e98b7c3fdce9eaaa2d
reuben-j <reubenjarvela@gmail.com>
2024-07-22 15:14:38 +0100

    encryptionFurther

--- passwordEncryptionTest.cpp
+++ passwordEncryptionTest.cpp
@@ -1,14 +1,26 @@
 #include <iostream> 
 #include <fstream>
+#include <ctime>
+#include <vector>
+#include <utility>
 using namespace std;
 
-string encrypt(string password) {
-    
-    for(int i = 0; i < password.length() + 1; i++) {
-        password[i] = password[i] + 2;
+pair<string, int>encrypt(string password) {
+    time_t now = time(nullptr);
+    int keyEnc = now % 10;
+    if(keyEnc == 0) {
+        keyEnc = 3;
     }
-    return password;
+    for(int i = 0; i < password.length() + 1; i++) {
+        if (password[i] >= 90) {
+        password[i] = password[i] - keyEnc;
+    } else {
+        password[i] = password[i] + keyEnc;
+    }
 }
+return make_pair(password, keyEnc);
+}
+
 
 string deEncrypt(string password) {
     for(int i = 0; i < password.length() + 1; i++) {
@@ -17,11 +29,24 @@ string deEncrypt(string password) {
     return password;
 }
 
-int main() {
+ int main() {
+
+time_t newNow = time(nullptr);
+
+
+
+
+
+
     cout << "What is the password you would like to encrypt? ";
     string password;
     cin >> password;
-    cout << "Password encrypted: " << encrypt(password) << endl;
-    string passWordEncrypted = encrypt(password);
-    cout << "Password de-encrypted: " << deEncrypt(passWordEncrypted);
+    
+    
+    
+
+pair<string, int> result = encrypt(password);
+string passwordEncrypted = result.first;
+
+cout << endl << "Password before encryption: " << password << endl << "After encryption: " << passwordEncrypted << endl << "Using the key of: " << result.second;
 }
