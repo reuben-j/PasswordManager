0b09c50a87e20e86352855135c3a9350e5fa60f6
reuben-j <reubenjarvela@gmail.com>
2024-07-22 14:22:19 +0100

    fileAppending

--- passwordManagerCPP.cpp
+++ passwordManagerCPP.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <ctype.h>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
 int main() {
-    cout << "1 to store new password.\n2 to retrieve saved password.\n";
+    cout << "1 to store new password.\n2 to retrieve saved password.\nAnswer: ";
     int answer;
+    string wantToRetrieve, wantToStore, nameToStore, passwordToStore;
     cin >> answer;
-    if (answer != 1 || answer != 2){
-        cin >> answer;
-    } else if (answer ==2) {
-        cout << "";
-    } else {
-        cout << "What is identifying name of the password you wish to store?"
+
+    
+    if (answer != 1 && answer != 2) {
+    cout << "Invalid answer";
+    return 0;
     }
     
+    cout << "What is the name of the password you would like to store?\n: ";
+    cin >> nameToStore;
+    cout << "What is the password for " << nameToStore << endl << ": ";
+    cin >> passwordToStore;
     
-    cout << answer;
+    ofstream passwordFile;
+    
+    // to append to file, use the ios_base::app function which prevents overwriting of file
+    passwordFile.open("passwordFile.txt", ios_base::app);
+    passwordFile << nameToStore << " - " << passwordToStore << endl;
+
+    passwordFile.close();
+    
 }
